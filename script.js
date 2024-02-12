9fee1180ed87d9d2bbf527340945842e6fb3af8d
Piermane <122611206+Piermane@users.noreply.github.com>
2024-02-12 15:13:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function createHearts() {
-    const numberOfHearts = 30; // кол-во сердечек
-    const pageHeight = Math.max(document.body.scrollHeight, window.innerHeight);
-    for (let i = 0; i < numberOfHearts; i++) {
+    const totalHearts = 20; // Количество седечек
+    const pageHeight = document.documentElement.scrollHeight;
+
+    for (let i = 0; i < totalHearts; i++) {
         let heart = document.createElement('div');
         heart.className = 'heart';
         heart.style.left = Math.random() * 100 + 'vw';
-        heart.style.top = Math.random() * 100 + 'vh'; 
+        heart.style.top = Math.random() * pageHeight + 'px';
         document.body.appendChild(heart);
     }
 }
